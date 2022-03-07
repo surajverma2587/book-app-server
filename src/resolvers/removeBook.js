d5efa3a4d25fac12e5d7e6e7a3bdd4f2c9e735d9
surajverma2587 <surajverma2587@gmail.com>
2022-03-07 20:10:30 +0000

    add remove book resolver

--- src/resolvers/removeBook.js
+++ src/resolvers/removeBook.js
@@ -1,3 +1,25 @@
-const removeBook = () => {};
+const { AuthenticationError, ApolloError } = require("apollo-server");
+
+const { User } = require("../models");
+
+const removeBook = async (_, { input }, context) => {
+  try {
+    if (context.user) {
+      const user = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: input.bookId } } },
+        { new: true }
+      );
+
+      return user;
+    }
+
+    console.log(`[ERROR]: Failed to remove book | User not logged in`);
+    throw new AuthenticationError("Failed to remove book");
+  } catch (error) {
+    console.log(`[ERROR]: Failed to remove book | ${error.message}`);
+    throw new ApolloError("Failed to remove book");
+  }
+};
 
 module.exports = removeBook;
