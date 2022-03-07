faf779e36f109813ac54d14804649661b1f648e3
surajverma2587 <surajverma2587@gmail.com>
2022-03-07 19:51:42 +0000

    add login mutation

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,46 +1,25 @@
 // import user model
-const { User } = require('../models');
+const { User } = require("../models");
 // import sign token function from auth
-const { signToken } = require('../utils/auth');
+const { signToken } = require("../utils/auth");
 
 module.exports = {
   // get a single user by either their id or their username
   async getSingleUser({ user = null, params }, res) {
     const foundUser = await User.findOne({
-      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+      $or: [
+        { _id: user ? user._id : params.id },
+        { username: params.username },
+      ],
     });
 
     if (!foundUser) {
-      return res.status(400).json({ message: 'Cannot find a user with this id!' });
+      return res
+        .status(400)
+        .json({ message: "Cannot find a user with this id!" });
     }
 
     res.json(foundUser);
-  },
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-  async createUser({ body }, res) {
-    const user = await User.create(body);
-
-    if (!user) {
-      return res.status(400).json({ message: 'Something is wrong!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
-  },
-  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-  // {body} is destructured req.body
-  async login({ body }, res) {
-    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
-    if (!user) {
-      return res.status(400).json({ message: "Can't find this user" });
-    }
-
-    const correctPw = await user.isCorrectPassword(body.password);
-
-    if (!correctPw) {
-      return res.status(400).json({ message: 'Wrong password!' });
-    }
-    const token = signToken(user);
-    res.json({ token, user });
   },
   // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
   // user comes from `req.user` created in the auth middleware function
@@ -66,7 +45,9 @@ module.exports = {
       { new: true }
     );
     if (!updatedUser) {
-      return res.status(404).json({ message: "Couldn't find user with this id!" });
+      return res
+        .status(404)
+        .json({ message: "Couldn't find user with this id!" });
     }
     return res.json(updatedUser);
   },
--- src/resolvers/login.js
+++ src/resolvers/login.js
@@ -1,3 +1,32 @@
-const login = () => {};
+const { ApolloError, AuthenticationError } = require("apollo-server");
+
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const login = async (_, { input }) => {
+  try {
+    const user = await User.findOne({ email: input.email });
+
+    if (!user) {
+      console.log(`[ERROR]: Failed to login | User does not exist`);
+      throw new AuthenticationError("Failed to login");
+    }
+
+    const isValidPassword = await user.isCorrectPassword(input.password);
+
+    if (!isValidPassword) {
+      console.log(`[ERROR]: Failed to login | Incorrect password`);
+      throw new AuthenticationError("Failed to login");
+    }
+
+    return {
+      token: signToken(user),
+      user,
+    };
+  } catch (error) {
+    console.log(`[ERROR]: Failed to login | ${error.message}`);
+    throw new ApolloError("Failed to login");
+  }
+};
 
 module.exports = login;
