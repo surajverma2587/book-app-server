b93ebb30e4562ea2ad91a260d904af7032830283
surajverma2587 <surajverma2587@gmail.com>
2022-03-07 20:01:10 +0000

    add me query resolver

--- src/resolvers/me.js
+++ src/resolvers/me.js
@@ -1,3 +1,26 @@
-const me = () => {};
+const { AuthenticationError, ApolloError } = require("apollo-server");
+
+const { User } = require("../models");
+
+const me = async (_, __, context) => {
+  try {
+    if (context.user) {
+      const user = await User.findById(context.user._id);
+
+      if (!user) {
+        console.log(`[ERROR]: Failed to get user | User does not exist`);
+        throw new AuthenticationError("Failed to get user");
+      }
+
+      return user;
+    }
+
+    console.log(`[ERROR]: Failed to get user | User not logged in`);
+    throw new AuthenticationError("Failed to get user");
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get user | ${error.message}`);
+    throw new ApolloError("Failed to get user");
+  }
+};
 
 module.exports = me;
