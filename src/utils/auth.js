3a2fbaf01afef17bd8c61ff090eaa1bf13e8826e
surajverma2587 <surajverma2587@gmail.com>
2022-03-07 19:33:02 +0000

    add graphql server and auth context

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,39 +1,36 @@
-const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require("apollo-server");
+const jwt = require("jsonwebtoken");
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = process.env.SECRET;
+const expiration = "2h";
+
+const signToken = ({ email, username, _id }) => {
+  const payload = { email, username, _id };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+};
+
+const authMiddleware = ({ req }) => {
+  let token = req.body.token || req.query.token || req.headers.authorization;
+
+  if (req.headers.authorization) {
+    token = token.split(" ").pop().trim();
+  }
+
+  if (!token) {
+    return req;
+  }
+
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+  } catch {
+    throw new AuthenticationError("Invalid token");
+  }
+
+  return req;
+};
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  signToken,
+  authMiddleware,
 };
